9a1f8124839be4f0ac6bf55a5b940972d64c7548
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2022-11-06 14:45:39 -0600

    update 2
    Se crearon los data sources, se almacenan en la base de datos de rrdtool y se grafica correctamente

--- 4. AdministraciónDeContabilidad/graphRRD.py
+++ 4. AdministraciónDeContabilidad/graphRRD.py
@@ -4,18 +4,34 @@ import time
 
 #Creaciòn de la grafica, y de igual forma se define el tiempo a graficar por el usuario
 
-
-tiempo_actual = int(time.time())
-#Grafica desde el tiempo actual menos diez minutos
-tiempo_inicial = tiempo_actual - 1200
-ret = rrdtool.graph( "traficoRED.png",
-                     "--start",str(tiempo_inicial),
-                     "--end",str(tiempo_actual),
-                     "--vertical-label=Bytes/s",
-                     "--title=Sistema de contabilidad \n Usando SNMP y RRDtools",
-                     "DEF:traficoEntrada=traficoRED.rrd:inoctets:AVERAGE",
-                     "DEF:traficoSalida=traficoRED.rrd:outoctets:AVERAGE",
-                     "CDEF:escalaIn=traficoEntrada,8,*",
-                     "CDEF:escalaOut=traficoSalida,8,*",
-                     "LINE3:escalaOut#0000FF:Tráfico de salida",#linea azul
-                     "LINE1:escalaIn#FF0000:Tráfico de entrada")#linea roja
+def graficar():
+    tiempo_actual = int(time.time())
+    #Grafica desde el tiempo actual menos diez minutos
+    tiempo_inicial = tiempo_actual - 600#1200
+    ret = rrdtool.graph( "traficoRED.png",
+                         "--start",str(tiempo_inicial),
+                         "--end",str(tiempo_actual),
+                         "--vertical-label=Bytes/s",
+                         "--title=Sistema de contabilidad \n Usando SNMP y RRDtools",
+                         "DEF:traficoEntrada=traficoRED.rrd:inoctets:AVERAGE",
+                         "DEF:traficoSalida=traficoRED.rrd:outoctets:AVERAGE",
+                         "DEF:packMulti=traficoRED.rrd:packmulticast:AVERAGE",
+                         "DEF:packIPV4=traficoRED.rrd:packrecibidosexito:AVERAGE",
+                         "DEF:icmpMensajes=traficoRED.rrd:respuestaicmp:AVERAGE",
+                         "DEF:segmentosEnvi=traficoRED.rrd:segmentosenviados:AVERAGE",
+                         "DEF:datagramaNoRe=traficoRED.rrd:datagramasnoreci:AVERAGE",
+                         "CDEF:escalaIn=traficoEntrada,8,*",
+                         "CDEF:escalaOut=traficoSalida,8,*",
+                         "CDEF:escala1=packMulti,8,*",
+                         "CDEF:escala2=packIPV4,8,*",
+                         "CDEF:escala3=icmpMensajes,8,*",
+                         "CDEF:escala4=segmentosEnvi,8,*",
+                         "CDEF:escala5=datagramaNoRe,8,*",
+                         "LINE1:escalaOut#0000FF:Tráfico de salida",#linea azul 3
+                         "LINE2:escalaIn#FF0000:Tráfico de entrada",#linea roja
+                         "LINE3:escalaIn#49ff00:Tráfico de packMulti",
+                         "LINE4:escalaIn#00ecff:Tráfico de packIPV4",
+                         "LINE5:escalaIn#ffff00:Tráfico de icmpMensajes",
+                         "LINE6:escalaIn#fb00ff:Tráfico de segMensajes",
+                         "LINE7:escalaIn#ff9300:Tráfico de dataNoRec",
+                         )
--- 4. AdministraciónDeContabilidad/updateRRD.py
+++ 4. AdministraciónDeContabilidad/updateRRD.py
@@ -1,6 +1,7 @@
 import time
 import rrdtool
 from getSNMP import consultaSNMP
+from graphRRD import graficar
 total_input_traffic = 0
 total_output_traffic = 0
 total_paquetes_multicast_recibidos = 0
@@ -12,39 +13,40 @@ total_de_datagramas_no_recibidos_por_puerto = 0
 mib ="1.3.6.1.2.1."
 paquetesMulticasInterfaz ="2.2.1.12"
 paquetesIPV4 ="4.9.0"
-ICMPMensajes = "5.9.0"
-ICMPMensajes2 = "5.22.0"
+ICMPMensajes = "5.9.0" #5.9
+#ICMPMensajes2 = "5.22.0"
 SegmentosEnviadosConexiones ="6.11.0"
-datagramasNoEntregados ="5.7.2.0"
+datagramasNoEntregados ="5.2.0"#5.7.2.0
 
 anchoBanda = ""
 paquetesTCP = ""
 datagramasUDP = ""
 
 def actualizarRrdtool():
+
     while 1:
         total_input_traffic = int( consultaSNMP('comunidadSNMP','localhost', '1.3.6.1.2.1.2.2.1.10.2'))
         total_output_traffic = int(consultaSNMP('comunidadSNMP','localhost','1.3.6.1.2.1.2.2.1.16.2'))
-        total_paquetes_multicast_recibidos = int( consultaSNMP('comunidadSNMP','localhost', mib+paquetesMulticasInterfaz))
+
+        consulta_multicast = consultaSNMP('comunidadSNMP', 'localhost', mib + paquetesMulticasInterfaz)
+        if consulta_multicast == "No":
+            total_paquetes_multicast_recibidos = 0
+        else:
+            total_paquetes_multicast_recibidos = int(consulta_multicast)
+
         total_paquetes_recibidos_exitosamente_ipv4 = int( consultaSNMP('comunidadSNMP','localhost', mib+paquetesIPV4))
         total_respuestas_ICMP_enviadas = int( consultaSNMP('comunidadSNMP','localhost', mib+ICMPMensajes))
         total_segmentos_enviados = int( consultaSNMP('comunidadSNMP','localhost', mib+SegmentosEnviadosConexiones))
+
         total_de_datagramas_no_recibidos_por_puerto = int( consultaSNMP('comunidadSNMP','localhost', mib+datagramasNoEntregados))
 
-        valor = "N:" + str(total_input_traffic) + ':' + str(total_output_traffic)
-        valor1 = "N:"+str(total_paquetes_multicast_recibidos)
-        valor2 = "N:"+str(total_paquetes_recibidos_exitosamente_ipv4)
-        valor3 = "N:"+str(total_respuestas_ICMP_enviadas)
-        valor4 = "N:"+str(total_segmentos_enviados)
-        valor5 = "N:"+str(total_de_datagramas_no_recibidos_por_puerto)
-        print (valor.center(10,"*"))
-        print(valor1.center(10, "*"))
-        print(valor2.center(10, "*"))
-        print(valor3.center(10, "*"))
-        print(valor4.center(10, "*"))
-        print(valor5.center(10, "*"))
-        rrdtool.update('traficoRED.rrd', valor,valor1,valor2,valor3,valor4,valor5)
+        valor = "N:" + str(total_input_traffic) + ':' + str(total_output_traffic)+":"+str(total_paquetes_multicast_recibidos)+":"+str(total_paquetes_recibidos_exitosamente_ipv4)+":"+str(total_respuestas_ICMP_enviadas)+":"+str(total_segmentos_enviados)+":"+str(total_de_datagramas_no_recibidos_por_puerto)
+
+        print (valor)
+
+        rrdtool.update('traficoRED.rrd', valor)
         rrdtool.dump('traficoRED.rrd','traficoRED.xml')
+        graficar()
         time.sleep(1)
 
     if ret:
