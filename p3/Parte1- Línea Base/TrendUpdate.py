7ee9eced561a1abfabd1e7ba912ebb31d7edeacf
Edgar García <58317087+EdgarGarciaMar@users.noreply.github.com>
2022-11-16 09:38:54 -0600

    Ejercicio miercoles

--- p3/Parte1- Línea Base/TrendUpdate.py
+++ p3/Parte1- Línea Base/TrendUpdate.py
@@ -5,8 +5,14 @@ rrdpath = '/home/edgar/Documents/GitHub/Redes3/p3/RRD/'
 carga_CPU = 0
 
 while 1:
-    carga_CPU = int(consultaSNMP('comunidadSNMP','localhost','1.3.6.1.2.1.25.3.3.1.2.196608'))
-    valor = "N:" + str(carga_CPU)
+    carga_CPU1 = int(consultaSNMP('comunidadSNMP','localhost','1.3.6.1.2.1.25.3.3.1.2.196608'))
+    carga_CPU2 = int(consultaSNMP('comunidadSNMP', 'localhost', '1.3.6.1.2.1.25.3.3.1.2.196609'))
+    carga_CPU3 = int(consultaSNMP('comunidadSNMP', 'localhost', '1.3.6.1.2.1.25.3.3.1.2.196610'))
+    carga_CPU4 = int(consultaSNMP('comunidadSNMP', 'localhost', '1.3.6.1.2.1.25.3.3.1.2.196611'))
+
+    suma = (carga_CPU1+carga_CPU2+carga_CPU3+carga_CPU4)/4
+
+    valor = "N:" + str(suma)
     print (valor)
     rrdtool.update(rrdpath+'trend.rrd', valor)
    # rrdtool.dump(rrdpath+'trend.rrd','trend.xml')
--- p3/Parte1- Línea Base/trendGraphDetection.py
+++ p3/Parte1- Línea Base/trendGraphDetection.py
@@ -5,34 +5,38 @@ import time
 rrdpath = '/home/edgar/Documents/GitHub/Redes3/p3/RRD/'
 imgpath = '/home/edgar/Documents/GitHub/Redes3/p3/IMG/'
 
-ultima_lectura = int(rrdtool.last(rrdpath+"trend.rrd"))
-tiempo_final = ultima_lectura
-tiempo_inicial = tiempo_final - 1800
-
-ret = rrdtool.graphv( imgpath+"deteccion.png",
+def generarGrafica(ultima_lectura):
+    tiempo_final = int(ultima_lectura)
+    tiempo_inicial = tiempo_final - 1800
+    ret = rrdtool.graphv( imgpath+"deteccion.png",
                      "--start",str(tiempo_inicial),
                      "--end",str(tiempo_final),
                      "--vertical-label=Cpu load",
                     '--lower-limit', '0',
                     '--upper-limit', '100',
-                    "--title=Carga del CPU del agente Usando SNMP y RRDtools \n Detección de umbrales",
+                    "--title=Carga del CPU del agente Usando SNMP y RRDtools \n Detección de umbrales Edgar Garcia",
                     "DEF:cargaCPU="+rrdpath+"trend.rrd:CPUload:AVERAGE",
                      "VDEF:cargaMAX=cargaCPU,MAXIMUM",
                      "VDEF:cargaMIN=cargaCPU,MINIMUM",
                      "VDEF:cargaSTDEV=cargaCPU,STDEV",
                      "VDEF:cargaLAST=cargaCPU,LAST",
-                 #   "CDEF:cargaEscalada=cargaCPU,8,*",
-                     "CDEF:umbral5=cargaCPU,5,LT,0,cargaCPU,IF",
+                     "CDEF:umbral50=cargaCPU,50,LT,0,cargaCPU,IF",
                      "AREA:cargaCPU#00FF00:Carga del CPU",
-                     "AREA:umbral5#FF9F00:Carga CPU mayor que 5",
-                     "HRULE:5#FF0000:Umbral 5%",
+                     "AREA:umbral50#FF9F00:Carga CPU mayor de 50",
+                     "HRULE:50#FF0000:Umbral  50%",
                      "PRINT:cargaLAST:%6.2lf",
                      "GPRINT:cargaMIN:%6.2lf %SMIN",
                      "GPRINT:cargaSTDEV:%6.2lf %SSTDEV",
                      "GPRINT:cargaLAST:%6.2lf %SLAST" )
-print (ret)
+    print (ret)
 
-ultimo_valor=float(ret['print[0]'])
-if ultimo_valor>5:
-    send_alert_attached("Sobrepasa Umbral línea base")
-    print("Sobrepasa Umbral línea base")
+while (1):
+    ultima_actualizacion = rrdtool.lastupdate(rrdpath + "trend.rrd")
+    timestamp=ultima_actualizacion['date'].timestamp()
+    dato=ultima_actualizacion['ds']["CPUload"]
+    print(dato)
+    if dato> 50:
+        generarGrafica(int(timestamp))
+        send_alert_attached("Sobrepasa el umbral Edgar Garcia Marciano")
+        print("sobrepasa el umbral")
+    time.sleep(20)
